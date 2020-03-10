43f8457fea3247afa59872ada3bec2c96eb5a1f8
chaeahpark <parkchaeah331@gmail.com>
2020-03-10 21:04:00 +0200

    Updates

---
+++ precache-manifest.c8a19012032ff3f92ca402e617a2a6c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "761f54c3b2cd3c2ac57390f8e0fa05f4",
+    "url": "/global-weather/index.html"
+  },
+  {
+    "revision": "776a22945d97c2072859",
+    "url": "/global-weather/static/css/main.a5c39931.chunk.css"
+  },
+  {
+    "revision": "b81f4a2de798ceba9337",
+    "url": "/global-weather/static/js/2.34edfef6.chunk.js"
+  },
+  {
+    "revision": "ddfd09a9397ea336a2550edb6da27f94",
+    "url": "/global-weather/static/js/2.34edfef6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "776a22945d97c2072859",
+    "url": "/global-weather/static/js/main.0e83b272.chunk.js"
+  },
+  {
+    "revision": "446fa684f9ffed3fc334",
+    "url": "/global-weather/static/js/runtime-main.f3c707aa.js"
+  }
+]);
--- src/apis/openweathermap.js
+++
@@ -1,5 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-  baseURL: 'https://cors-anywhere.herokuapp.com/http://api.openweathermap.org'
-});
